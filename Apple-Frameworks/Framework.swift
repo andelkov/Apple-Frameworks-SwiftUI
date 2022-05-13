89d680926ed3fc677e13d23e13b91c3bb8a90c29
andelkov <andelkov@gmail.com>
2022-05-14 00:51:40 +0200

    Added "Details view"

--- Apple-Frameworks/Framework.swift
+++ Apple-Frameworks/Framework.swift
@@ -17,10 +17,10 @@ struct Framework: Hashable, Identifiable {
 
 struct MockData: Hashable {
     
-    static let sampleFramework = Framework(name: "ARKit",
-                                           imageName: "arkit",
-                                           urlString: "https://developer.apple.com/augmented-reality/arkit",
-                                           description: "ARKit 4 introduces a brand-new Depth API, creating a new way to access the detailed depth information gathered by the LiDAR Scanner on iPhone 12 Pro, iPhone 12 Pro Max, and iPad Pro.\n\nLocation Anchors leverages the higher-resolution data in Apple Maps to place AR experiences at a specific point in the world in your iPhone and iPad apps. And support for face tracking extends to all devices with the Apple Neural Engine and a front-facing camera, so even more users can experience the joy of AR in photos and videos.")
+    static let sampleFramework = Framework(name: "CarPlay",
+                                  imageName: "carplay",
+                                  urlString: "https://developer.apple.com/carplay",
+                                  description: "A smarter, safer way to use your iPhone in the car, CarPlay takes the things you want to do with your iPhone while driving and puts them on your car’s built-in display.\n\nYou can get directions, make calls, send and receive messages, and listen to music, all while staying focused on the road.")
     
     static let frameworks = [
         Framework(name: "App Clips",
---
+++ Apple-Frameworks/FrameworkDetailView.swift
@@ -0,0 +1,54 @@
+//
+//  FrameworkDetailView.swift
+//  Apple-Frameworks
+//
+//  Created by Anđelko on 10.May.22.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    
+    var framework: Framework
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color(.label))
+                        .imageScale(.medium)
+                        .frame(width: 44, height: 44)
+                }
+            }
+            .padding()
+            
+            Spacer()
+            
+            FrameworkTitleView(framework: framework)
+            
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                AFButton(title: "Learn more")
+            }
+        }
+    }
+}
+
+struct FrameworkDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkDetailView(framework: MockData.sampleFramework)
+        .preferredColorScheme(.dark)
+    }
+}
